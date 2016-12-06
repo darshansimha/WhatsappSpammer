7bc6f96c361eb4f57bc9e99254bcf9d5a44c04aa
Darshan Simha U <darshansimhau@gmail.com>
2016-12-06 09:58:12 -0600

    Added some silicon valley flair to the program

--- spammer.js
+++ spammer.js
@@ -11,20 +11,21 @@ function bindTheEvent(target, eventType, char) {
 }
 
 //Main function
-function spam(){
+function spam(spamText){
   if (counter<100){
-    bindTheEvent(ourInputElement, "textInput", "Bhains");
+    bindTheEvent(ourInputElement, "textInput", spamText);
     var input = document.getElementsByClassName("icon btn-icon icon-send");
     input[0].click();// Clicks the send button
     counter++;
     setTimeout(spam,1);
   }
   else { 
-    bindTheEvent(ourInputElement, "textInput", "Bhains");
+    bindTheEvent(ourInputElement, "textInput", spamText);
     var input = document.getElementsByClassName("icon btn-icon icon-send");
     input[0].click();
   }
 }
 
 //Call the main method
-spam();
+var spamTextToSend = "It's not magic, it is talent and sweat"; //Enter your text here
+spam(spamTextToSend);
